37c7f709ba0744fabc58bc26a07092bf6f2afc0b
Tomster <tomster@emberjs.com>
2015-03-30 12:55:03 -0500

    Initial Commit from Ember CLI v0.2.1
                                                            _...,
                                                         ,:^;,...;
                  -+===;.         ,,--++====++-,,,     .:  /.....,
                :::::~+++++#:,+#++++++++++++++++++#*..:   /,......
               (,,,,,,::=+++##++++++++++++++++++++++#.   :....../
                ...,,,,,::++++++++++++++++++++++++++++++*..,...:
                *..+...,#@@@@@@@@@++++++++++++++++++++++#*....*
                  @#,;##############@@@+*+#@@@@@@@@@@#*++#..<
                  *@##@@+,-*^^^*-+@####@@@######@@@#####@@,,,+
                    @#@*            @#@@@@#@@+--*^^*--#@@@@@@#
                    @#@.    @#      @##+++@#,           .@@#@@
                     #@#    @@     +@@++++#@@     @@     :@@
                     :@#*         @#@++++++@#*    #@     @@+
                    :*+@@#;,.__.+@#@+,-^^.++@#          @@++
                   ;*  :*@@@##@@@@;++r._j^.+@##@+,.__,,@@++.
                  /*    ........+++++++++++++#@@@@@###@@#++++,
                ,:       ...,@@@#++===----==@@@####,,....+++++
               .:       ......@@##@\   ;   :@####@,,...... +++.
               ;       .........@###,   ;  ;xx#@;,,.....   *;+,
               |       ........,*;xxxx--^--=xxx,........   :+#;
               ;         ......,,;xxxxxxxxxxxxx;,.....     *+#
                ;          ......,::xxxx;.     ......       +.   .
                 *;            .........  +###  ....       / ,. /:| ,.
                   .+:             ...  ;##++##, .      ,#. (..v..;*./
                      **                ##  ###*    .:*&&&+. \.,....<,
                       #&+**==-..,,__  ;##  ###  :,*+&&&&&&&v+#&,,.._/
                      #&&&&*...,::,,.  ##; ,##* .*****;:&&&&&&&&&
                     ,+*+;~*..*** *.* ### ###* *******    *+#&;*
                                      ##,;##    ****    :,  **
             #####    ##   ###  ###,  ########       .#####   ;##  ##
            #######  ;##  #### ,###.  ##########    ########  ### ####
           ###  ###  ### ##########   ####  ####   ,##   ###  #######*
          ### ,###  ##############:   ##     ###  #### ,##   :#### ###  ##;
      ##########    ########### ##   .##    ,###  #######    ##### :######
        ######    .###### ####  ##   ###    ### ######*    :#####   ####
           #############  ####  ################    ######## ###
            #####*  *#*    #:   :###   *###*         *####    #*

---
+++ Brocfile.js
@@ -0,0 +1,35 @@
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var pickFiles   = require('broccoli-static-compiler');
+var mergeTrees  = require('broccoli-merge-trees');
+var vulcanize = require('broccoli-vulcanize');
+
+var app = new EmberApp();
+
+var polymerVulcanize = vulcanize('app', {
+  input: 'elements.html',
+  output: 'assets/vulcanized.html',
+  csp: true,
+  inline: true,
+  strip: false,
+  excludes: {
+    imports: ["(^data:)|(^http[s]?:)|(^\/)"],
+    scripts: ["(^data:)|(^http[s]?:)|(^\/)"],
+    styles: ["(^data:)|(^http[s]?:)|(^\/)"]
+  }
+});
+
+var polymer = pickFiles('bower_components/', {
+  srcDir: '',
+  files: [
+    'webcomponentsjs/webcomponents.js',
+    'polymer/polymer.js',
+    'polymer/polymer.html'
+  ],
+  destDir: '/assets'
+});
+
+module.exports = mergeTrees([
+  polymerVulcanize,
+  polymer,
+  app.toTree()
+]);
